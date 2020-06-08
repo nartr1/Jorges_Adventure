7448b31dfa14efa49a14ec209f62ce0d07227cfa
nartr1 <nartr@mail.com>
2020-06-08 17:30:38 -0600

    Adding the basics!

---
+++ GAME_TESTING/build/linux/gmake/obj/x64/Debug/GAME_TESTING.d
@@ -0,0 +1,186 @@
+obj/x64/Debug/GAME_TESTING.o: ../../../src/GAME_TESTING.cpp \
+ ../../../include/orx.h ../../../include/orxInclude.h \
+ ../../../include/base/orxModule.h ../../../include/base/orxVersion.h \
+ ../../../include/base/orxType.h ../../../include/base/orxDecl.h \
+ ../../../include/base/orxBuild.h ../../../include/orxKernel.h \
+ ../../../include/anim/orxAnim.h ../../../include/orxInclude.h \
+ ../../../include/object/orxStructure.h ../../../include/core/orxClock.h \
+ ../../../include/core/orxSystem.h ../../../include/math/orxVector.h \
+ ../../../include/debug/orxDebug.h ../../../include/memory/orxMemory.h \
+ ../../../include/math/orxMath.h ../../../include/utils/orxString.h \
+ ../../../include/utils/orxLinkList.h ../../../include/utils/orxTree.h \
+ ../../../include/anim/orxAnimPointer.h \
+ ../../../include/anim/orxAnimSet.h ../../../include/anim/orxAnim.h \
+ ../../../include/anim/orxAnimSet.h ../../../include/core/orxClock.h \
+ ../../../include/core/orxCommand.h ../../../include/core/orxConfig.h \
+ ../../../include/core/orxConsole.h ../../../include/display/orxFont.h \
+ ../../../include/display/orxTexture.h \
+ ../../../include/display/orxDisplay.h \
+ ../../../include/plugin/orxPluginCore.h \
+ ../../../include/plugin/orxPluginType.h \
+ ../../../include/plugin/define/orxPlugin_CoreDefine.h \
+ ../../../include/plugin/define/orxPlugin_Display.h \
+ ../../../include/plugin/define/orxPlugin_CoreID.h \
+ ../../../include/plugin/define/orxPlugin_Joystick.h \
+ ../../../include/plugin/define/orxPlugin_Keyboard.h \
+ ../../../include/plugin/define/orxPlugin_Mouse.h \
+ ../../../include/plugin/define/orxPlugin_Physics.h \
+ ../../../include/plugin/define/orxPlugin_Render.h \
+ ../../../include/plugin/define/orxPlugin_SoundSystem.h \
+ ../../../include/math/orxOBox.h ../../../include/memory/orxBank.h \
+ ../../../include/utils/orxHashTable.h ../../../include/io/orxInput.h \
+ ../../../include/io/orxJoystick.h ../../../include/io/orxKeyboard.h \
+ ../../../include/io/orxMouse.h ../../../include/core/orxEvent.h \
+ ../../../include/core/orxLocale.h ../../../include/core/orxResource.h \
+ ../../../include/core/orxSystem.h ../../../include/core/orxThread.h \
+ ../../../include/debug/orxDebug.h ../../../include/debug/orxFPS.h \
+ ../../../include/debug/orxProfiler.h ../../../include/core/orxThread.h \
+ ../../../include/display/orxGraphic.h \
+ ../../../include/display/orxDisplay.h ../../../include/display/orxFont.h \
+ ../../../include/display/orxScreenshot.h \
+ ../../../include/display/orxText.h ../../../include/display/orxTexture.h \
+ ../../../include/io/orxFile.h ../../../include/io/orxInput.h \
+ ../../../include/io/orxJoystick.h ../../../include/io/orxKeyboard.h \
+ ../../../include/io/orxMouse.h ../../../include/main/orxParam.h \
+ ../../../include/math/orxAABox.h ../../../include/math/orxMath.h \
+ ../../../include/math/orxOBox.h ../../../include/math/orxVector.h \
+ ../../../include/memory/orxBank.h ../../../include/memory/orxMemory.h \
+ ../../../include/object/orxFrame.h ../../../include/object/orxFX.h \
+ ../../../include/object/orxObject.h \
+ ../../../include/display/orxGraphic.h ../../../include/sound/orxSound.h \
+ ../../../include/sound/orxSoundSystem.h \
+ ../../../include/object/orxFXPointer.h ../../../include/object/orxFX.h \
+ ../../../include/object/orxObject.h ../../../include/object/orxSpawner.h \
+ ../../../include/object/orxFrame.h \
+ ../../../include/object/orxStructure.h \
+ ../../../include/object/orxTimeLine.h \
+ ../../../include/plugin/orxPlugin.h \
+ ../../../include/plugin/orxPluginCore.h \
+ ../../../include/plugin/orxPluginUser.h \
+ ../../../include/physics/orxBody.h ../../../include/physics/orxPhysics.h \
+ ../../../include/math/orxAABox.h ../../../include/physics/orxPhysics.h \
+ ../../../include/render/orxCamera.h ../../../include/render/orxRender.h \
+ ../../../include/render/orxViewport.h \
+ ../../../include/render/orxCamera.h \
+ ../../../include/render/orxShaderPointer.h \
+ ../../../include/render/orxShader.h ../../../include/render/orxShader.h \
+ ../../../include/render/orxShaderPointer.h \
+ ../../../include/render/orxViewport.h ../../../include/sound/orxSound.h \
+ ../../../include/sound/orxSoundPointer.h \
+ ../../../include/sound/orxSoundSystem.h ../../../include/orxUtils.h \
+ ../../../include/utils/orxHashTable.h \
+ ../../../include/utils/orxLinkList.h ../../../include/utils/orxString.h \
+ ../../../include/utils/orxTree.h
+../../../include/orx.h:
+../../../include/orxInclude.h:
+../../../include/base/orxModule.h:
+../../../include/base/orxVersion.h:
+../../../include/base/orxType.h:
+../../../include/base/orxDecl.h:
+../../../include/base/orxBuild.h:
+../../../include/orxKernel.h:
+../../../include/anim/orxAnim.h:
+../../../include/orxInclude.h:
+../../../include/object/orxStructure.h:
+../../../include/core/orxClock.h:
+../../../include/core/orxSystem.h:
+../../../include/math/orxVector.h:
+../../../include/debug/orxDebug.h:
+../../../include/memory/orxMemory.h:
+../../../include/math/orxMath.h:
+../../../include/utils/orxString.h:
+../../../include/utils/orxLinkList.h:
+../../../include/utils/orxTree.h:
+../../../include/anim/orxAnimPointer.h:
+../../../include/anim/orxAnimSet.h:
+../../../include/anim/orxAnim.h:
+../../../include/anim/orxAnimSet.h:
+../../../include/core/orxClock.h:
+../../../include/core/orxCommand.h:
+../../../include/core/orxConfig.h:
+../../../include/core/orxConsole.h:
+../../../include/display/orxFont.h:
+../../../include/display/orxTexture.h:
+../../../include/display/orxDisplay.h:
+../../../include/plugin/orxPluginCore.h:
+../../../include/plugin/orxPluginType.h:
+../../../include/plugin/define/orxPlugin_CoreDefine.h:
+../../../include/plugin/define/orxPlugin_Display.h:
+../../../include/plugin/define/orxPlugin_CoreID.h:
+../../../include/plugin/define/orxPlugin_Joystick.h:
+../../../include/plugin/define/orxPlugin_Keyboard.h:
+../../../include/plugin/define/orxPlugin_Mouse.h:
+../../../include/plugin/define/orxPlugin_Physics.h:
+../../../include/plugin/define/orxPlugin_Render.h:
+../../../include/plugin/define/orxPlugin_SoundSystem.h:
+../../../include/math/orxOBox.h:
+../../../include/memory/orxBank.h:
+../../../include/utils/orxHashTable.h:
+../../../include/io/orxInput.h:
+../../../include/io/orxJoystick.h:
+../../../include/io/orxKeyboard.h:
+../../../include/io/orxMouse.h:
+../../../include/core/orxEvent.h:
+../../../include/core/orxLocale.h:
+../../../include/core/orxResource.h:
+../../../include/core/orxSystem.h:
+../../../include/core/orxThread.h:
+../../../include/debug/orxDebug.h:
+../../../include/debug/orxFPS.h:
+../../../include/debug/orxProfiler.h:
+../../../include/core/orxThread.h:
+../../../include/display/orxGraphic.h:
+../../../include/display/orxDisplay.h:
+../../../include/display/orxFont.h:
+../../../include/display/orxScreenshot.h:
+../../../include/display/orxText.h:
+../../../include/display/orxTexture.h:
+../../../include/io/orxFile.h:
+../../../include/io/orxInput.h:
+../../../include/io/orxJoystick.h:
+../../../include/io/orxKeyboard.h:
+../../../include/io/orxMouse.h:
+../../../include/main/orxParam.h:
+../../../include/math/orxAABox.h:
+../../../include/math/orxMath.h:
+../../../include/math/orxOBox.h:
+../../../include/math/orxVector.h:
+../../../include/memory/orxBank.h:
+../../../include/memory/orxMemory.h:
+../../../include/object/orxFrame.h:
+../../../include/object/orxFX.h:
+../../../include/object/orxObject.h:
+../../../include/display/orxGraphic.h:
+../../../include/sound/orxSound.h:
+../../../include/sound/orxSoundSystem.h:
+../../../include/object/orxFXPointer.h:
+../../../include/object/orxFX.h:
+../../../include/object/orxObject.h:
+../../../include/object/orxSpawner.h:
+../../../include/object/orxFrame.h:
+../../../include/object/orxStructure.h:
+../../../include/object/orxTimeLine.h:
+../../../include/plugin/orxPlugin.h:
+../../../include/plugin/orxPluginCore.h:
+../../../include/plugin/orxPluginUser.h:
+../../../include/physics/orxBody.h:
+../../../include/physics/orxPhysics.h:
+../../../include/math/orxAABox.h:
+../../../include/physics/orxPhysics.h:
+../../../include/render/orxCamera.h:
+../../../include/render/orxRender.h:
+../../../include/render/orxViewport.h:
+../../../include/render/orxCamera.h:
+../../../include/render/orxShaderPointer.h:
+../../../include/render/orxShader.h:
+../../../include/render/orxShader.h:
+../../../include/render/orxShaderPointer.h:
+../../../include/render/orxViewport.h:
+../../../include/sound/orxSound.h:
+../../../include/sound/orxSoundPointer.h:
+../../../include/sound/orxSoundSystem.h:
+../../../include/orxUtils.h:
+../../../include/utils/orxHashTable.h:
+../../../include/utils/orxLinkList.h:
+../../../include/utils/orxString.h:
+../../../include/utils/orxTree.h:
